c53da8022f0db4a38e967af74b4ba7d02941fd9b
ljelectrar <ljelectrar@gmail.com>
2024-04-03 12:06:00 -0300

    Partial finalization

--- src/main.rs
+++ src/main.rs
@@ -82,9 +82,14 @@ fn main()  -> Result<(), Box<dyn Error>> {
             }
         }
 
+        // UPDATES
         player.update(delta);
         if invaders.update(delta) {
             audio.play("move");
+        }
+
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
         }
 
         // Draw & Render
@@ -98,6 +103,18 @@ fn main()  -> Result<(), Box<dyn Error>> {
 
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // WIN OR LOSE
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
+
     }
 
     // Cleanup
